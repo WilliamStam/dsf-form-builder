3b93faffce8dac50e217c38e4a388ee77e40fa16
William Stam <william@munsoft.co.za>
2024-09-24 13:18:38 +0200

    hhh

--- src/lib/items/input/index.ts
+++ src/lib/items/input/index.ts
@@ -4,9 +4,7 @@ import DateItem from "./date"
 import NumberItem from "./number"
 import SelectItem from "./select"
 import TextItem from "./text"
-// import InputRegistery from "./input"
-// import SelectRegistry from "./select"
-// import ContentRegistry from "./content"
+
 const items = new ItemRegistry(new Group({label: "Inputs"}));
 items.registerItem(DateItem)
 items.registerItem(NumberItem)
